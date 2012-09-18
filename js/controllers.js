13f60782f8eb1e1866024ce4dc94ac951d8009c2
nsoeffers <nsoeffers@gmail.com>
2012-09-18 04:21:22 -0400

    Introducing requirejs module system

--- js/controllers.js
+++ js/controllers.js
@@ -1,17 +1,21 @@
-function AccountListCtrl($scope, $window) {    
-    $scope.accounts = [];  
-};
-
-function AccountDetailCtrl($scope, $window, $timeout) {  
-    $scope.save = function() {
-        $scope.showSuccessMessage = true;
-        $timeout(function() {
-            $scope.showSuccessMessage = false;
-        }, 1000);
+define('controllers', ['angular'], function(angular) {
+    
+    return { 
+        AccountListCtrl : function ($scope, $window) {    
+            $scope.accounts = [];  
+        },
+    
+        AccountDetailCtrl : function ($scope, $window, $timeout) {  
+            $scope.save = function() {
+                $scope.showSuccessMessage = true;
+                $timeout(function() {
+                    $scope.showSuccessMessage = false;
+                }, 1000);
+            };
+        
+            $scope.account = { name: ''};
+            
+            $scope.showSuccessMessage = false;    
+        }
     };
-    
-    $scope.account = { name: ''};
-    
-    $scope.showSuccessMessage = false;    
-    
-};
+});
---
+++ js/main.js
@@ -0,0 +1,39 @@
+requirejs.config({
+    paths: {
+        'jquery'    : 'https://ajax.googleapis.com/ajax/libs/jquery/1.8.1/jquery.min',
+        'angular'   : 'https://ajax.googleapis.com/ajax/libs/angularjs/1.0.2/angular.min',
+        'bootstrap' : 'https://netdna.bootstrapcdn.com/twitter-bootstrap/2.1.1/js/bootstrap.min',
+        'i18next'   : 'i18next-1.5.6.min'
+    },
+    shim: {
+        'angular': {
+            //These script dependencies should be loaded before loading
+            //backbone.js
+            deps: ['jquery'],
+            exports: 'angular'
+        },
+        'bootstrap': {
+            deps: ['jquery'],
+            exports: 'bootstrap'
+        },
+        'i18next' : {
+            deps: ['jquery'],
+            exports: '$.i18n'
+        }
+    }
+});
+
+require([ 'jquery', 'angular', 'bootstrap', 'i18next', 'controllers', 'domReady'], function($, angular, bootstrap, i18next, controllers) {
+    angular.module('nsoFinance', [])
+        .controller('AccountListCtrl', controllers.AccountListCtrl)
+        .controller('AccountDetailCtrl', controllers.AccountDetailCtrl);
+    i18next.init({   ns: { namespaces: ['translation'], defaultNs: 'translation'},
+                    lng: 'nl',
+                    fallbackLng: 'nl',
+                    resGetPath: 'locales/__lng__/__ns__.json',
+                    useLocalStorage: false,
+                    debug: true
+                }, function() {
+                    $('*[data-i18n]').i18n();
+                });    
+});
